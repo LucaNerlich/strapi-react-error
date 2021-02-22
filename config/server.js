fa602d878770b85f558b46804fcffe11c06a9f22
Luca Nerlich <nerlich@adobe.com>
2021-02-22 22:19:42 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f291cf2082d5b0eddd04abc6cf35843d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '296876ae-89f5-4142-aa1f-b9436a063a1f'
+};
